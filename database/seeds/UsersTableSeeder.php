460e51558d41417260fcaa65a84841f573605a66
SatoMayu <hanon1742@gmail.com>
2022-08-22 11:10:17 +0900

    UsersTableとSubjectTableに初期データ登録

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            'over_name' => '野比',
+            'under_name' => 'のび太',
+            'over_name_kana' => 'ノビ',
+            'under_name_kana' => 'ノビタ',
+            'mail_address' => 'nobita@nnn',
+            'sex' => '1',
+            'birth_day' => '1994-08-07',
+            'role' => '4',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
