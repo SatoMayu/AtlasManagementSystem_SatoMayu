cc83d17cca14dd804640777be0c4e408bf3b7d88
SatoMayu <hanon1742@gmail.com>
2022-10-12 12:23:38 +0900

    classの追加

--- resources/views/authenticated/bulletinboard/post_create.blade.php
+++ resources/views/authenticated/bulletinboard/post_create.blade.php
@@ -7,7 +7,7 @@
       <p class="mb-0">カテゴリー</p>
       <select class="w-100" form="postCreate" name="post_category_id">
         @foreach($main_categories as $main_category)
-            <optgroup label="{{ $main_category->main_category }}"></optgroup>
+            <optgroup label="{{ $main_category->main_category }}" class="main_category"></optgroup>
         <!-- サブカテゴリー表示 -->
                 @foreach($main_category->subCategories as $sub_category)
                 <!-- ↑↑($sub_categories as $sub_category)としてしまうと全てのメインカテゴリーのオプションに全てのサブカテゴリーが表示されてしまう -->
