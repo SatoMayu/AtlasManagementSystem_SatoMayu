3e91380ddf7cf19ec592c33166ab98e9198e658c
SatoMayu <hanon1742@gmail.com>
2022-11-08 07:41:54 +0900

    予約キャンセルのモーダルの記述

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -10,6 +10,39 @@
         {!! $calendar->render() !!}
       </div>
     </div>
+
+    <!-- キャンセルボタンのモーダル -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-test modal-test-centered modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <!-- 投稿編集フォーム開始 -->
+          <div class="modal-body">
+            <form action="" method="POST">
+              @csrf
+              <!-- 変更した投稿内容の受け渡し -->
+              <p>予約日</p>
+              <p>時間</p>
+              <p>上記の予約をキャンセルしてもよろしいですか？</p>
+              <!-- 変更した投稿のidの受け渡し -->
+              <!-- <input type="hidden" name="" class="modal_id" placeholder="aiu"value=""></input>-->
+              
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+              </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
     <div class="text-right w-75 m-auto">
       <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
     </div>
