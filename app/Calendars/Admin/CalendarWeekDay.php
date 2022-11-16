c60cef87679f2051dafb8012071fb815c8a0bfd4
SatoMayu <hanon1742@gmail.com>
2022-11-16 16:00:05 +0900

    no message

--- app/Calendars/Admin/CalendarWeekDay.php
+++ app/Calendars/Admin/CalendarWeekDay.php
@@ -31,14 +31,13 @@ class CalendarWeekDay{
 
     $html[] = '<div class="text-left">';
     if($one_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="{{ route('calendar.admin.detail',['date' => '.$ymd.'],['part' => '1']) }}">1部</a>'.$one_part->users()->count().'</p>';
-
+      $html[] = '<p class="day_part m-0 pt-1"><a href=" '.route('calendar.admin.detail',['date' => $one_part->setting_reserve,'part' => $one_part->setting_part,'id' => $one_part->id]).' ">1部</a>'.$one_part->users()->count().'</p>';
     }
     if($two_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="/calendar/{id}/{data}/{part?}">2部</a>'.$two_part->users()->count().'</p>';
+      $html[] = '<p class="day_part m-0 pt-1"><a href=" '.route('calendar.admin.detail',['date' => $two_part->setting_reserve,'part' => $two_part->setting_part,'id' => $two_part->id]).' ">2部</a>'.$two_part->users()->count().'</p>';
     }
     if($three_part){
-      $html[] = '<p class="day_part m-0 pt-1"><a href="/calendar/{id}/{data}/{part?}">3部</a>'.$three_part->users()->count().'</p>';
+      $html[] = '<p class="day_part m-0 pt-1"><a href=" '.route('calendar.admin.detail',['date' => $three_part->setting_reserve,'part' => $three_part->setting_part,'id' => $three_part->id]).' ">3部</a>'.$three_part->users()->count().'</p>';
     }
     $html[] = '</div>';
 
